b824e4c4a106d73c842a38758addde52d94e976a
Armin Namavari <ajnamavari12@gmail.com>
2016-11-13 19:21:24 -0800

    Add corresponding hdf5 parsing file

--- ngrams_feature_extractor.py
+++ ngrams_feature_extractor.py
@@ -1,5 +1,6 @@
 import sklearn
-
+from hdf5_getters import *
+import os
 
 def make_train_pair(filename):
     h5 = open_h5_file_read(filename)
@@ -8,6 +9,14 @@ def make_train_pair(filename):
     pitch_diffs = [pitches[i] - pitches[i - 1] for i in xrange(1, len(pitches))]
     h5.close()
     return {'title': title, 'pitch_diffs': pitch_diffs}
+
+for root, dirs, files in os.walk('data'):
+    files = glob.glob(os.path.join(root, '*h5'))
+    for f in files:
+        train_pair = make_train_pair(f)
+        titles.append(train_pair['title'])
+        pitch_diff_list.append(train_pair['pitch_diffs'])
+
 # some lines omitted
 neigh = NearestNeighbors(n_neighbors=1) # predict the closest song
 # a title list is also maintained
